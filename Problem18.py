cab119f4e6d31358ab69412a6c23c80db43143a0
Ryan <mullin8888@gmail.com>
2019-05-31 12:41:50 -0600

    Problem 18 Solved

--- Problem18.py
+++ Problem18.py
@@ -18,4 +18,14 @@ def rowMax(a,b):
         a[i] = max(  (a[i] + b[i]) , (a[i] + b[i+1]) )
     return a
 
+def triangleMax(triangle):
+    row = len(triangle) - 2
+    
+    while len(triangle[row]) > 1:
+        triangle[row] = rowMax(triangle[row], triangle[row + 1])
+        row -= 1
+    return rowMax(triangle[0],triangle[1])[0]
+    
+ans = triangleMax(nums)
 
+print("Answer is: " + str(ans))
