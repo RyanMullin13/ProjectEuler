9bcd25b257ba2c036ed8f47acf468a871ae0b17f
Ryan <mullin8888@gmail.com>
2019-06-02 07:46:12 -0600

    Implement Letter Number Dictionary

--- SolvedProblems/Problem22.py
+++ SolvedProblems/Problem22.py
@@ -5,10 +5,22 @@ Created on Sun Jun  2 07:25:59 2019
 
 @author: ryan
 """
+from string import ascii_uppercase
 
 file = open("Problem22Data.txt")
 
 for i in file:
     names = i.split(",")
-for i in range(len(names) - 1):
+for i in range(len(names)):
     names[i] = names[i].strip('"')
+
+names.sort()
+
+letters = []
+for i in ascii_uppercase:
+    letters.append(i)
+nums = list(range(1,27))
+
+value = dict(zip(letters,nums))
+
+print(value['A'])
