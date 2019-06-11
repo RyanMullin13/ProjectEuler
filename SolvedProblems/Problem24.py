3f4c443d29a3d03080710e5ee5a914170e3948cb
Ryan <mullin8888@gmail.com>
2019-06-11 14:43:34 -0600

    Fix Problem 24 Message

--- SolvedProblems/Problem24.py
+++ SolvedProblems/Problem24.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 11 14:22:54 2019
+Created on Tue Jun 11 14:35:49 2019
 
 @author: Ryan
 """
 
 from itertools import permutations
 
-perm = list(permutations([0,1,2,3,4,5,6,7,8,9]))
+perms = list(permutations([0,1,2,3,4,5,6,7,8,9]))
+target = perms[1000000 - 1]
 
-target = perm[1000000 - 1]
-
-answer = ''
+answer = ""
 for i in target:
     answer += str(i)
 print("Answer is: " + answer)
